24bcd35646cbe8fc04e3a11c55dec7c6d9d25c8f
Connor <36247578+UmbraSang@users.noreply.github.com>
2020-04-21 17:12:21 -0700

    Update WorldMap.java
    changed from string array to arraylist<String>

--- src/main/GamePackage/WorldMap.java
+++ src/main/GamePackage/WorldMap.java
@@ -1,11 +1,12 @@
 package GamePackage;
 
 
+import java.util.ArrayList;
 
 public class WorldMap {
 
     private Board game;
-    private String[] cityList; //String list of city names //TODO:refactor into game object. remove here?
+    private ArrayList<String> cityList; //String list of city names //TODO:refactor into game object. remove here?
     private int[][] adjacency; //Adjacency matrix of cities. 1 is touching.
     private Node[] nodeMap; //Array of nodes that have references to nodes they're touching.
 
@@ -19,11 +20,11 @@ public class WorldMap {
 
 
     public Node[] makeMap() {
-        Node[] nodeList = new Node[cityList.length];
-        for(int i=0; i<cityList.length; i++){
-            nodeList[i] = new Node(cityList[i], game.colourAssign(i)); //TODO: review if this should call game.getCityList every time? my heart says no.
+        Node[] nodeList = new Node[cityList.size()];
+        for(int i=0; i<cityList.size(); i++){
+            nodeList[i] = new Node(cityList.get(i), game.colourAssign(i)); //TODO: review if this should call game.getCityList every time? my heart says no.
         }
-        for (int i=0; i<cityList.length; i++){
+        for (int i=0; i<cityList.size(); i++){
            for (int j=0; j<adjacency[i].length; j++) {
                if (adjacency[i][j] == 1) {
                    nodeList[i].addNext(nodeList[j]);
